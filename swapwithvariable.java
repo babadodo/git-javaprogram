750eb50a72f6ea93ba75a3e2a60b3c2c44516c2a
babadodo <shasank.dixit124@gmail.com>
2018-09-11 00:47:07 +0530

    swapwithvariable

---
+++ swapwithvariable.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class swapwithvariable
+{
+public static void main(String args[])
+{
+int  x,y,temp;
+System.out.println("Enter x and y");
+Scanner in = new Scanner(System.in);
+
+x = in.nextInt();
+y = in.nextInt();
+
+System.out.println(" Before Swapping :\nX : " +x+ "\nY : " +y);
+
+ temp = x;
+ x = y;
+ y = temp;
+
+ System.out.println("After Swapping :\nX : " +x+ "\nY : " +y);
+
+
+}
+}
